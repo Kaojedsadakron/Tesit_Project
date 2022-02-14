863eff1f5d19d1dc0604ffeab04e5b1ad42ecb53
Kittiphum <kittiphum.phengyim@gmail.com>
2022-02-14 19:32:39 +0700

    เพิ่มการ คำนวณ ผลรวม submission

--- src/app/conclusion/conclusion.component.ts
+++ src/app/conclusion/conclusion.component.ts
@@ -1,76 +1,91 @@
 import { Component, OnInit } from '@angular/core';
 import { Papa } from 'ngx-papaparse';
-import { CrudService} from '../service/crud.service';
-import {AngularFirestore} from '@angular/fire/compat/firestore'
+import { CrudService } from '../service/crud.service';
+import { AngularFirestore } from '@angular/fire/compat/firestore'
 import { user } from '../interface/user';
 import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { runInThisContext } from 'vm';
+import { submission } from '../interface/submission';
 @Component({
-  
+
   selector: 'app-conclusion',
   templateUrl: './conclusion.component.html',
   styleUrls: ['./conclusion.component.css']
 })
 export class ConclusionComponent implements OnInit {
-  userId:any;
-  userName:any;
-  userAge:any;
-  userAddress:any;
+  userId: any;
+  userName: any;
+  userAge: any;
+  userAddress: any;
   data = [{}]
   finalData: any
+  sumSubmission = [0,0,0,0,0];
 
-  score:any = [];
-  sum:any = [];
-  constructor(private service:CrudService) {}
+
+  score: any = [];
+  sum: any = [];
+  constructor(private service: CrudService) { }
   ngOnInit(): void {
   }
-  
-  createUser(evt:any){
-    // this.service.addDoc(this.userName,this.userAge,this.userAddress)
+
+  createUser(evt: any) {
     this.finalData = this.service.getData()
     var files = evt.target.files; // FileList object
     var subject_count = 5
-    var final_score = [0,0,0,0,0] 
-    Object.values(this.finalData).forEach((element:any) => {
-      element.text().then((text:any,reject:any) => {
-        console.log("resolve",text)
+    var final_score = [0, 0, 0, 0, 0]
+    Object.values(this.finalData).forEach((element: any) => {
+      element.text().then((text: any, reject: any) => {
+        console.log("resolve", text)
         text = text.split(",")
-        for(var i = 1 ; i <= subject_count ; i++) {
-          final_score[i-1] += parseInt(text[i])
+        for (var i = 1; i <= subject_count; i++) {
+          final_score[i - 1] += parseInt(text[i])
         }
-      }).then(()=> {
-        
-      })
-      console.log("final",final_score)
-    });
-   
-    let data = [{}]
-    console.log("type ",this.finalData)
-  }
-  
+      }).then(() => {
 
-  handleFileSelect(evt:any) {
+      })
+      console.log("final", final_score)
+    });
+
+    let data = [{}]
+    console.log("type ", this.finalData)
+  }
+
+
+  handleFileSelect(evt: any) {
     var files = evt.target.files; // FileList object
     var subject_count = 5
-    var final_score = [0,0,0,0,0] 
-    Object.values(files).forEach((element:any) => {
-      element.text().then((text:any,reject:any) => {
-        console.log("resolve",text)
+    var final_score = [0, 0, 0, 0, 0]
+    Object.values(files).forEach((element: any) => {
+      element.text().then((text: any, reject: any) => {
+        console.log("resolve", text)
         text = text.split(",")
-        for(var i = 1 ; i <= subject_count ; i++) {
-          final_score[i-1] += parseInt(text[i])
+        for (var i = 1; i <= subject_count; i++) {
+          final_score[i - 1] += parseInt(text[i])
         }
-      }).then(()=> {
-        
+      }).then(() => {
+
       })
-      console.log("final",final_score)
+      console.log("final", final_score)
     });
-   
+
   }
 
-  sumOfSubject(score:any){
+  test() {
+    this.service.getFinaldata().subscribe(res => {
+      let resArray: submission[] = res
+      resArray.forEach(value => {
+        for (let i = 0 ; i<value.data.length ; i++){
+          this.sumSubmission[i] = +value.data[i] + this.sumSubmission[i]
+        }
+      })
+      console.log("value " , this.sumSubmission )
+    });
+
+  }
+
+  sumOfSubject(score: any) {
     for (let i = 0; i < score.length; i++) {
-      console.log('test '+score)
+      console.log('test ' + score)
       // if(score[i] == 10){
       //   this.sum.push('ชอบเเละหลงไหลวิชานี้มากๆ')
       // }else
@@ -88,7 +103,7 @@ export class ConclusionComponent implements OnInit {
       // }
     }
   }
-  
+
 
 
 
