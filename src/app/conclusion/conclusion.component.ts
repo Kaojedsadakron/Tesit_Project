7787a8836520470e1eadd074155662d7a7288f43
BarameeChomphoo <baramee.chomphoo@gmail.com>
2022-02-14 19:44:39 +0700

    แสดงผล

--- src/app/conclusion/conclusion.component.ts
+++ src/app/conclusion/conclusion.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Papa } from 'ngx-papaparse';
 import { CrudService } from '../service/crud.service';
-import { AngularFirestore } from '@angular/fire/compat/firestore'
-import { user } from '../interface/user';
-import { analyzeAndValidateNgModules } from '@angular/compiler';
-import { runInThisContext } from 'vm';
 import { submission } from '../interface/submission';
 @Component({
 
@@ -13,64 +8,16 @@ import { submission } from '../interface/submission';
   styleUrls: ['./conclusion.component.css']
 })
 export class ConclusionComponent implements OnInit {
-  userId: any;
-  userName: any;
-  userAge: any;
-  userAddress: any;
   data = [{}]
   finalData: any
   sumSubmission = [0,0,0,0,0];
-
-
   score: any = [];
   sum: any = [];
   constructor(private service: CrudService) { }
   ngOnInit(): void {
   }
 
-  createUser(evt: any) {
-    this.finalData = this.service.getData()
-    var files = evt.target.files; // FileList object
-    var subject_count = 5
-    var final_score = [0, 0, 0, 0, 0]
-    Object.values(this.finalData).forEach((element: any) => {
-      element.text().then((text: any, reject: any) => {
-        console.log("resolve", text)
-        text = text.split(",")
-        for (var i = 1; i <= subject_count; i++) {
-          final_score[i - 1] += parseInt(text[i])
-        }
-      }).then(() => {
-
-      })
-      console.log("final", final_score)
-    });
-
-    let data = [{}]
-    console.log("type ", this.finalData)
-  }
-
-
-  handleFileSelect(evt: any) {
-    var files = evt.target.files; // FileList object
-    var subject_count = 5
-    var final_score = [0, 0, 0, 0, 0]
-    Object.values(files).forEach((element: any) => {
-      element.text().then((text: any, reject: any) => {
-        console.log("resolve", text)
-        text = text.split(",")
-        for (var i = 1; i <= subject_count; i++) {
-          final_score[i - 1] += parseInt(text[i])
-        }
-      }).then(() => {
-
-      })
-      console.log("final", final_score)
-    });
-
-  }
-
-  test() {
+  getSumOfSubject() {
     this.service.getFinaldata().subscribe(res => {
       let resArray: submission[] = res
       resArray.forEach(value => {
