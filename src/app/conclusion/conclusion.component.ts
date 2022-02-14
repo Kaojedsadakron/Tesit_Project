6d82d5d60fb320a1d75888012671ecc07dbd44cd
Kittiphum <kittiphum.phengyim@gmail.com>
2022-02-14 19:41:02 +0700

    เพิ่มการแสดง ความชอบของแต่ ละวิชา

--- src/app/conclusion/conclusion.component.ts
+++ src/app/conclusion/conclusion.component.ts
@@ -78,30 +78,31 @@ export class ConclusionComponent implements OnInit {
           this.sumSubmission[i] = +value.data[i] + this.sumSubmission[i]
         }
       })
-      console.log("value " , this.sumSubmission )
+      
+      this.sumOfSubject(this.sumSubmission)
     });
 
   }
 
-  sumOfSubject(score: any) {
+  sumOfSubject(score: Array<number>) {
     for (let i = 0; i < score.length; i++) {
-      console.log('test ' + score)
-      // if(score[i] == 10){
-      //   this.sum.push('ชอบเเละหลงไหลวิชานี้มากๆ')
-      // }else
-      // if(score[i] <= 9 && score[i] >= 7){
-      //   this.sum.push('ชอบวิชานี้')
-      // }else
-      // if(score[i] <= 6 && score[i] >= 4){
-      //   this.sum.push('ชอบหรือไม่ชอบก็ได้แต่ก็ยังพอใช้ได้ในวิชานี้แต่ก็ยังไม่ได้ดี')
-      // }else
-      // if(score[i] <= 3 && score[i] > 0){
-      //   this.sum.push('ค่อนข้างไม่ชอบวิชานี้')
-      // }else
-      // if(score[i] == 0){
-      //   this.sum.push('ไม่ชอบวิชานี้มากๆ')
-      // }
+      if(score[i] == 10){
+        this.sum.push('ชอบเเละหลงไหลวิชานี้มากๆ')
+      }else
+      if(score[i] <= 9 && score[i] >= 7){
+        this.sum.push('ชอบวิชานี้')
+      }else
+      if(score[i] <= 6 && score[i] >= 4){
+        this.sum.push('ชอบหรือไม่ชอบก็ได้แต่ก็ยังพอใช้ได้ในวิชานี้แต่ก็ยังไม่ได้ดี')
+      }else
+      if(score[i] <= 3 && score[i] > 0){
+        this.sum.push('ค่อนข้างไม่ชอบวิชานี้')
+      }else
+      if(score[i] == 0){
+        this.sum.push('ไม่ชอบวิชานี้มากๆ')
+      }
     }
+    console.log('score ' + this.sum)
   }
 
 
