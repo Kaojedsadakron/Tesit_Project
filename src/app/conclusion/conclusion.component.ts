0bf22f096d8d22f62738298ce77038133a64ca05
BarameeChomphoo <baramee.chomphoo@gmail.com>
2022-02-15 11:25:58 +0700

    เปลี่ยนการแสดงผล

--- src/app/conclusion/conclusion.component.ts
+++ src/app/conclusion/conclusion.component.ts
@@ -12,20 +12,25 @@ export class ConclusionComponent implements OnInit {
   finalData: any
   sumSubmission = [0,0,0,0,0];
   score: any = [];
+  dataWeek:any;
+  resArray!: submission[]
   sum: any = [];
+  sumInterest:any = [];
+  sumHundred:any = [];
   constructor(private service: CrudService) { }
   ngOnInit(): void {
+    this.getSumOfSubject();
   }
 
   getSumOfSubject() {
     this.service.getFinaldata().subscribe(res => {
-      let resArray: submission[] = res
-      resArray.forEach(value => {
+      this.resArray = res
+      this.resArray.forEach(value => {
         for (let i = 0 ; i<value.data.length ; i++){
           this.sumSubmission[i] = +value.data[i] + this.sumSubmission[i]
         }
       })
-      
+      this.dataWeek=this.resArray.length;
       this.sumOfSubject(this.sumSubmission)
     });
 
@@ -35,21 +40,42 @@ export class ConclusionComponent implements OnInit {
     for (let i = 0; i < score.length; i++) {
       if(score[i] == 10){
         this.sum.push('ชอบเเละหลงไหลวิชานี้มากๆ')
+        this.sumInterest.push('สามารถพัฒนาในวิชานี้ได้แน่นอน')
+        this.sumHundred.push(score[i]*10);
       }else
-      if(score[i] <= 9 && score[i] >= 7){
-        this.sum.push('ชอบวิชานี้')
+      if(score[i] <= 9 && score[i] >= 8){
+        this.sum.push('ค่อนข้างชอบวิชานี้อย่างมาก')
+        this.sumInterest.push('ค่อนข้างที่จะสามารถพัฒนาในวิชานี้ได้อย่างมาก')
+        this.sumHundred.push(score[i]*10);
       }else
-      if(score[i] <= 6 && score[i] >= 4){
-        this.sum.push('ชอบหรือไม่ชอบก็ได้แต่ก็ยังพอใช้ได้ในวิชานี้แต่ก็ยังไม่ได้ดี')
+      if(score[i] <= 7 && score[i] >= 6){
+        this.sum.push('ค่อนข้างชอบวิชานี้')
+        this.sumInterest.push('ค่อนข้างที่จะสามารถพัฒนาในวิชานี้ได้')
+        this.sumHundred.push(score[i]*10);
       }else
-      if(score[i] <= 3 && score[i] > 0){
+      if(score[i] <= 5 && score[i] >= 4){
+        this.sum.push('ชอบหรือไม่ชอบก็ได้')
+        this.sumInterest.push('อาจจะสามารถพัฒนาในวิชานี้ได้')
+        this.sumHundred.push(score[i]*10);
+      }else
+      if(score[i] <= 3 && score[i] >= 2){
         this.sum.push('ค่อนข้างไม่ชอบวิชานี้')
+        this.sumInterest.push('ค่อนข้างสามารถพัฒนาในวิชานี้ได้ยาก')
+        this.sumHundred.push(score[i]*10);
+      }else
+      if(score[i] == 1){
+        this.sum.push('ค่อนข้างไม่ชอบวิชานี้อย่างมาก')
+        this.sumInterest.push('ค่อนข้างสามารถพัฒนาในวิชานี้ได้ยากอย่างมาก')
+        this.sumHundred.push(score[i]*10);
       }else
       if(score[i] == 0){
-        this.sum.push('ไม่ชอบวิชานี้มากๆ')
+        this.sum.push('ไม่ชอบวิชานี้เลย')
+        this.sumInterest.push('สามารถพัฒนาในวิชานี้ได้ยากมาก')
+        this.sumHundred.push(score[i]*10);
       }
     }
     console.log('score ' + this.sum)
+    console.log('wwwwwww' + this.sumHundred)
   }
 
 
